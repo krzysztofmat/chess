c3a6a3b79045be56af69a45a5c1cc16e69b1c1bb
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-04-02 15:40:26 +0200

    fix build after tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import postcss from './postcss.config.js';
+import postcss from './postcss.config';
 
 // https://vite.dev/config/
 export default defineConfig({
