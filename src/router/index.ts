cc499351c3a07c2815eb387a485b49ea843bca2b
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-04-12 12:03:55 +0200

    return to use of router

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/play/computer',
+      name: 'play-computer',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../components/PlayComputer.vue'),
+    },
   ],
 });
 
